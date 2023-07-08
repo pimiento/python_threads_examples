cdbe9acd09dd44681fb102b8c3d809a299019da3
Pavel Vavilin <shtartora@gmail.com>
2023-07-08 20:46:33 +0500

    more for Celery

--- project/demoapp/tasks.py
+++ project/demoapp/tasks.py
@@ -1,4 +1,6 @@
 # Create your tasks here
+from typing import Union, List, Tuple
+from django.core.mail import send_mail
 from celery import shared_task
 
 
@@ -22,3 +24,8 @@ def countdown(n=int(5e8)):
     for _ in n:
         n -= 1
     return n
+
+@shared_task
+def send_notification(subj: str, text: str, send_from: str, send_to: Union[List[str], Tuple[str]]):
+    send_mail(subj, text, send_from, send_to, fail_silently=False)
+    return "SENT"
--- project/project/settings.py
+++ project/project/settings.py
@@ -83,6 +83,8 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = "/tmp/"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
